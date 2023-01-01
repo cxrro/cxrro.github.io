676e58bbd146c320b08d31cf259306de1cdc1832
carro <cqz@u.northwestern.edu>
2023-01-01 10:39:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,25 +14,25 @@ window.onload = () => {
 	l2.setAttribute("data-content", "<h1>resume</h1>last updated dec 2022<br><a href='https://drive.google.com/file/d/14GFlCptTqRZT8a1rikGfvZTIgrqbaSa-/preview'>view</a>");
 
 	const l3 = document.querySelector("#l3");
-	l3.setAttribute("data-content", "<h1>recent fun stuff</h1>visited christkindlmarket<br><br>learned to bake brioche over thanksgiving break<br><br>spent ~4 months learning about gamecube controller modding and made a custom gcc for my partner<br><br>spent ~6 hours teaching myself advanced tableau");
+	l3.setAttribute("data-content", "<h1>recent fun stuff</h1>nov 2022 --learned to bake brioche<br><br>oct 2022 -- made a custom gcc for my partner<br><br>sep 2022 -- taught myself advanced tableau");
 
 	const l4 = document.querySelector("#l4");
 	l4.setAttribute("data-content", "<h1>bun</h1><img src='https://i.imgur.com/G0f9JFF.gif' alt='animated image of the drawing tool in use. the user selects the brushes and draws the body, ears, and face of a cartoon bunny'><a href='https://bun.carro.dev'><br>view</a><br>a project for cs396: generative methods which uses p5js to make brushes for people to easily create bunnies (or other creatures). the project uses different techniques, including beginShape and endShape, delayed stacked shapes, randomly generated faces, and color / brush size pickers.");
 
 	const l5 = document.querySelector("#l5");
-	l5.setAttribute("data-content", "<h1>site</h1>the site you're on right now, created from scratch as an assignment for cs330: human-computer interaction without the use of any external frameworks / libraries<br><br>when you expand a content square, it'll move to the center of the grid and swap places with the square that was previously in the center. you can also click outside of the squares to see them all<br><br>you can also navigate this site without a mouse<br><br>shoutout to openai chat for debugging my code lol<br><br><a href='https://github.com/cxrro/cxrro.github.io'>github repo</a>");
+	l5.setAttribute("data-content", "<h1>site</h1>the site you're on right now, created from scratch as an assignment for cs330: human-computer interaction without the use of any external frameworks / libraries<br><br>when you expand a content square, it'll move to the center of the grid and swap places with the square that was previously in the center. you can also click outside of the squares to see them all<br><br>you can also navigate this site without a mouse<br><br><a href='https://github.com/cxrro/cxrro.github.io'>github repo</a>");
 
 	const l6 = document.querySelector("#l6");
-	l6.setAttribute("data-content", "<h1>work</h1>i am in my productivity era<br><br><a href='https://arch.library.northwestern.edu/collections/707957731'>comm studies honors thesis<a><br><a href='https://advising.soc.northwestern.edu/academic_modules/media-arts-and-game-design/'>media arts and game design capstone<a><br><a href='https://wiki.communitydata.science/Ecology_of_Online_Communities/Community_rules_study'>cdsc rules study<a><br><a href='https://studio.knightlab.com/projects/live-streaming/'>knight lab livestreaming<a><br><a href='https://magic.gg/news/magic-30-tickets-and-events-details#Program'>wotc project management intern<a><br><a href='https://discord.gg/forensics'>competitive debate discord<a>");
+	l6.setAttribute("data-content", "<h1>work</h1>current projects:<br><br><a href='https://arch.library.northwestern.edu/collections/707957731'>comm studies honors thesis<a><br><a href='https://advising.soc.northwestern.edu/academic_modules/media-arts-and-game-design/'>media arts and game design capstone<a><br><a href='https://wiki.communitydata.science/Ecology_of_Online_Communities/Community_rules_study'>cdsc rules study<a><br><a href='https://studio.knightlab.com/projects/live-streaming/'>knight lab livestreaming<a><br><a href='https://magic.gg/news/magic-30-tickets-and-events-details#Program'>wotc project management intern<a><br><a href='https://discord.gg/forensics'>competitive debate discord<a>");
 
 	const l7 = document.querySelector("#l7");
-	l7.setAttribute("data-content", "<h1>social</h1>haha content<br><br><a href='https://twitch.tv/carroh'>twitch<a><a href='https://are.na/carro'>are.na<a><a href='https://www.linkedin.com/in/czou/'>linkedin<a><a href='https://twitter.com/carrohno'>twitter<a><a href='https://carro.substack.com'>substack<a>");
+	l7.setAttribute("data-content", "<h1>socials</h1><a href='https://twitch.tv/carroh'>twitch<a><a href='https://are.na/carro'>are.na<a><a href='https://www.linkedin.com/in/czou/'>linkedin<a><a href='https://twitter.com/cqzou'>twitter<a><a href='https://carro.substack.com'>substack<a>");
 
 	const l8 = document.querySelector("#l8");
-	l8.setAttribute("data-content", "<h1>stuff i like</h1><br><br><a href='https://www.sunsama.com'>sunsama<a><a href='https://reboothq.substack.com'>reboot<a><a href='https://teamfighttactics.leagueoflegends.com'>tft<a><a href='https://www.twitch.tv/moonlimes'>moonlimes<a>");
+	l8.setAttribute("data-content", "<h1>stuff i like</h1><br><br><a href='https://www.sunsama.com'>sunsama<a><a href='https://reboothq.substack.com'>reboot<a><a href='https://teamfighttactics.leagueoflegends.com'>tft<a>");
 
 	const l9 = document.querySelector("#l9");
-	l9.setAttribute("data-content", "<h1>contact</h1>email: cqz@u.northwestern.edu<br>discord: carro#0001");
+	l9.setAttribute("data-content", "<h1>contact</h1>email: cqz@u.northwestern.edu");
 
 	// Select all of the squares on the page
 	const squares = document.querySelectorAll('.square');
